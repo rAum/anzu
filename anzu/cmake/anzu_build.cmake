023ce1109f3156ccfb6bc9a1757fb672da4b360a
Piotr Gródek <ipc0nfg@gmail.com>
2020-01-06 17:25:46 +0100

    Add install and export for anzu library

--- anzu/cmake/anzu_build.cmake
+++ anzu/cmake/anzu_build.cmake
@@ -7,7 +7,12 @@
 
 # This is a common target which contains flags that should be used to build anzu projects.
 # It is strongly requested to use this target only as PRIVATE dependency to prevent leaks.
-add_library(anzu_build_flags INTERFACE)
+# Now, why INTERFACE IMPORTED?
+# INTERFACE - means we don't need to compile or link anything, OBJECT has different semantic.
+# IMPORTED - this is only set because if cmake is really stubborn and errors when one try to
+#            export a library which link to this target, even if it is private..
+#            see https://gitlab.kitware.com/cmake/cmake/issues/17357
+add_library(anzu_build_flags INTERFACE IMPORTED)
 
 #TODO: Flags are for gcc/clang. Extend for other compilers.
 option(ANZU_WITH_WARNINGS "Turns on/off warnings" ON)
@@ -20,4 +25,16 @@ option(ANZU_WITH_WERROR "Turns on/off treating warnings as errors" OFF)
 if (ANZU_WITH_WERROR AND NOT WIN32)
     message("Warnings as errors are enabled.")
     target_compile_options(anzu_build_flags INTERFACE -Werror)
+endif()
+
+# TODO(rAum): think of what can be enabled on msvc
+option(ANZU_NATIVE_RELEASE_BUILD "Turns on/off native build optimization flags" OFF)
+if(ANZU_NATIVE_RELEASE_BUILD)
+    if (NOT WIN32)
+        message("Native build is enabled")
+        set(ReleaseFlags "-march=native -mtune=native")
+        target_compile_options(anzu_build_flags INTERFACE $<$<CONFIG:RELEASE>:${ReleaseFlags}>)
+    else()
+        message(WARNING "Currently only non-windows platforms are supported for ANZU_NATIVE_RELEASE_BUILD")
+    endif()
 endif()
